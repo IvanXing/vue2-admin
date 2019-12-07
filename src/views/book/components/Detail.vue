eb0ff079878b7b1fe483ecec4a30b0dca2e320f2
sam <247765564@qq.com>
2019-12-07 16:23:36 +0800

    电子书新增和编辑

--- src/views/book/components/Detail.vue
+++ src/views/book/components/Detail.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-form ref="postForm" :model="postForm">
+  <el-form ref="postForm" :model="postForm" :rules="rules">
     <sticky :class-name="'sub-navbar'">
       <el-button v-if="!isEdit" @click="showGuide">显示帮助</el-button>
       <el-button
@@ -30,7 +30,7 @@
           </el-form-item>
           <el-row>
             <el-col :span="12">
-              <el-form-item label="作者：" :label-width="labelWidth">
+              <el-form-item prop="author" label="作者：" :label-width="labelWidth">
                 <el-input
                   v-model="postForm.author"
                   placeholder="作者"
@@ -38,7 +38,7 @@
               </el-form-item>
             </el-col>
             <el-col :span="12">
-              <el-form-item label="出版社：" :label-width="labelWidth">
+              <el-form-item prop="publisher" label="出版社：" :label-width="labelWidth">
                 <el-input
                   v-model="postForm.publisher"
                   placeholder="出版社"
@@ -48,7 +48,7 @@
           </el-row>
           <el-row>
             <el-col :span="12">
-              <el-form-item label="语言：" :label-width="labelWidth">
+              <el-form-item prop="language" label="语言：" :label-width="labelWidth">
                 <el-input
                   v-model="postForm.language"
                   placeholder="语言"
@@ -56,7 +56,7 @@
               </el-form-item>
             </el-col>
             <el-col :span="12">
-              <el-form-item label="根文件：" :label-width="labelWidth">
+              <el-form-item prop="rootFile" label="根文件：" :label-width="labelWidth">
                 <el-input
                   v-model="postForm.rootFile"
                   placeholder="根文件"
@@ -67,7 +67,7 @@
           </el-row>
           <el-row>
             <el-col :span="12">
-              <el-form-item label="文件路径：" :label-width="labelWidth">
+              <el-form-item prop="filePath" label="文件路径：" :label-width="labelWidth">
                 <el-input
                   v-model="postForm.filePath"
                   placeholder="文件路径"
@@ -76,7 +76,7 @@
               </el-form-item>
             </el-col>
             <el-col :span="12">
-              <el-form-item label="解压路径：" :label-width="labelWidth">
+              <el-form-item prop="unzipPath" label="解压路径：" :label-width="labelWidth">
                 <el-input
                   v-model="postForm.unzipPath"
                   placeholder="解压路径"
@@ -87,18 +87,18 @@
           </el-row>
           <el-row>
             <el-col :span="12">
-              <el-form-item label="封面路径：" :label-width="labelWidth">
+              <el-form-item prop="coverPath" label="封面路径：" :label-width="labelWidth">
                 <el-input
-                  v-model="postForm.filePath"
+                  v-model="postForm.coverPath"
                   placeholder="封面路径"
                   disabled
                 />
               </el-form-item>
             </el-col>
             <el-col :span="12">
-              <el-form-item label="文件名称：" :label-width="labelWidth">
+              <el-form-item prop="originalName" label="文件名称：" :label-width="labelWidth">
                 <el-input
-                  v-model="postForm.unzipPath"
+                  v-model="postForm.originalName"
                   placeholder="文件名称"
                   disabled
                 />
@@ -107,7 +107,7 @@
           </el-row>
           <el-row>
             <el-col :span="24">
-              <el-form-item label="封面：" :label-width="labelWidth">
+              <el-form-item prop="cover" label="封面：" :label-width="labelWidth">
                 <a v-if="postForm.cover" :href="postForm.cover" target="_blank">
                   <img :src="postForm.cover" class="preview-img">
                 </a>
@@ -118,8 +118,8 @@
           <el-row>
             <el-col :span="24">
               <el-form-item label="目录：" :label-width="labelWidth">
-                <div v-if="postForm.contents && postForm.contents.length > 0" class="contents-wrapper">
-                  <el-tree />
+                <div v-if="contentsTree && contentsTree.length > 0" class="contents-wrapper">
+                  <el-tree :data="contentsTree" @node-click="onContentClick" />
                 </div>
                 <span v-else>无</span>
               </el-form-item>
@@ -136,6 +136,14 @@ import Sticky from '../../../components/Sticky'
 import Warning from './Warning'
 import EbookUpload from '../../../components/EbookUpload'
 import MdInput from '../../../components/MDinput'
+import { createBook, getBook, updateBook } from '../../../api/book'
+
+const fields = {
+  title: '书名',
+  author: '作者',
+  publisher: '出版社',
+  language: '语言'
+}
 
 export default {
   components: { MdInput, Sticky, Warning, EbookUpload },
@@ -143,27 +151,135 @@ export default {
     isEdit: Boolean
   },
   data() {
+    const validateRequire = (rule, value, callback) => {
+      if (value.length === 0) {
+        callback(new Error(fields[rule.field] + '必须填写'))
+      } else {
+        callback()
+      }
+    }
     return {
       loading: false,
-      postForm: {
-        ebook_uri: ''
-      },
+      postForm: {},
       fileList: [],
-      labelWidth: '120px'
+      labelWidth: '120px',
+      contentsTree: [],
+      rules: {
+        title: [{ validator: validateRequire }],
+        author: [{ validator: validateRequire }],
+        language: [{ validator: validateRequire }],
+        publisher: [{ validator: validateRequire }]
+      }
+    }
+  },
+  created() {
+    if (this.isEdit) {
+      const fileName = this.$route.params.fileName
+      this.getBookData(fileName)
     }
   },
   methods: {
-    onUploadSuccess() {
-      console.log('onUploadSuccess')
+    getBookData(fileName) {
+      getBook(fileName).then(response => {
+        this.setData(response.data)
+      })
+    },
+    onContentClick(data) {
+      console.log(data)
+      if (data.text) {
+        window.open(data.text)
+      }
+    },
+    setDefault() {
+      // this.postForm = Object.assign({}, defaultForm)
+      this.contentsTree = []
+      this.fileList = []
+      this.$refs.postForm.resetFields()
+    },
+    setData(data) {
+      const {
+        title,
+        author,
+        publisher,
+        language,
+        rootFile,
+        cover,
+        url,
+        originalName,
+        contents,
+        contentsTree,
+        fileName,
+        coverPath,
+        filePath,
+        unzipPath
+      } = data
+      this.postForm = {
+        ...this.postForm,
+        title,
+        author,
+        publisher,
+        language,
+        rootFile,
+        cover,
+        url,
+        originalName,
+        contents,
+        contentsTree,
+        fileName,
+        coverPath,
+        filePath,
+        unzipPath
+      }
+      this.contentsTree = contentsTree
+      this.fileList = [{ name: originalName || fileName, url }]
+    },
+    onUploadSuccess(data) {
+      console.log('onUploadSuccess', data)
+      this.setData(data)
     },
     onUploadRemove() {
-      console.log('onUploadRemove')
+      this.setDefault()
     },
     submitForm() {
-      this.loading = true
-      setTimeout(() => {
+      const onSuccess = (response) => {
+        const { msg } = response
+        this.$notify({
+          title: '操作成功',
+          message: msg,
+          type: 'success',
+          duration: 2000
+        })
         this.loading = false
-      }, 1000)
+      }
+
+      if (!this.loading) {
+        this.loading = true
+        this.$refs.postForm.validate((valid, fields) => {
+          console.log(valid, fields)
+          if (valid) {
+            const book = Object.assign({}, this.postForm)
+            delete book.contentsTree
+            if (!this.isEdit) {
+              createBook(book).then(response => {
+                onSuccess(response)
+                this.setDefault()
+              }).catch(() => {
+                this.loading = false
+              })
+            } else {
+              updateBook(book).then(response => {
+                onSuccess(response)
+              }).catch(() => {
+                this.loading = false
+              })
+            }
+          } else {
+            const message = fields[Object.keys(fields)[0]][0].message
+            this.$message({ message, type: 'error' })
+            this.loading = false
+          }
+        })
+      }
     },
     showGuide() {
       console.log('show guide...')
@@ -175,6 +291,7 @@ export default {
 <style lang="scss" scoped>
   .detail-container {
     padding: 40px 50px 20px;
+
     .preview-img {
       width: 200px;
       height: 270px;
