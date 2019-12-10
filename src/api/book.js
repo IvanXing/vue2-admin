58b167f06315187ffc19111f7eeb40dd7203938e
sam <247765564@qq.com>
2019-12-10 21:12:32 +0800

    电子书列表

--- src/api/book.js
+++ src/api/book.js
@@ -23,3 +23,27 @@ export function getBook(fileName) {
     params: { fileName }
   })
 }
+
+export function getCategory() {
+  return request({
+    url: '/book/category',
+    method: 'get'
+  })
+}
+
+export function listBook(params) {
+  return request({
+    url: '/book/list',
+    method: 'get',
+    params
+  })
+}
+
+export function deleteBook(fileName) {
+  return request({
+    url: '/book/delete',
+    method: 'get',
+    params: { fileName }
+  })
+}
+
